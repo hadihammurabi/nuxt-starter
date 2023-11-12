6fbcb7a669e7746443d2b4d1dd357495fd6c2268
hadihammurabi <hadihammurabi@gmail.com>
2023-11-12 09:18:16 +0700

    feat(uno): load webfonts and use transformerDirectives

--- uno.config.ts
+++ uno.config.ts
@@ -1,10 +1,21 @@
 import { defineConfig } from 'unocss';
 import presetUno from '@unocss/preset-uno';
 import presetIcons from '@unocss/preset-icons';
+import presetWebFonts from '@unocss/preset-web-fonts'
+import transformerDirectives from '@unocss/transformer-directives';
 
 export default defineConfig({
   presets: [
     presetUno(),
     presetIcons(),
+    presetWebFonts({
+      provider: 'google',
+      fonts: {
+        sans: 'Poppins',
+      },
+    }),
+  ],
+  transformers: [
+    transformerDirectives(),
   ],
 });
