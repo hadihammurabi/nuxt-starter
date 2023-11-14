f2dd9415bee76127b5f428a1f01661711b17673a
hadihammurabi <hadihammurabi@gmail.com>
2023-11-14 15:41:45 +0700

    fix(index): show chart using vue chartjs

--- pages/index.vue
+++ pages/index.vue
@@ -10,11 +10,44 @@ const onSheetBottomNotifToggle = () => {
   sheetBottomNotifShow.value = !sheetBottomNotifShow.value;
 };
 
+import { Bar } from 'vue-chartjs'
+import { Chart as ChartJS, Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale } from 'chart.js'
+
+ChartJS.register(Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale)
+
+const chartData = {
+  labels: [
+    'January',
+    'February',
+    'March',
+    'April',
+    'May',
+    'June',
+    'July',
+    'August',
+    'September',
+    'October',
+    'November',
+    'December'
+  ],
+  datasets: [
+    {
+      label: 'Data One',
+      backgroundColor: '#f87979',
+      data: [40, 20, 12, 39, 10, 40, 39, 50, 40, 20, 12, 11]
+    }
+  ]
+}
+
+const chartOptions = {
+  responsive: true,
+  maintainAspectRatio: false
+}
 </script>
 
 <template>
   <BarTop @profile-click="onSheetBottomProfileToggle" @notif-click="onSheetBottomNotifToggle" />
-  <h1>welcome</h1>
+  <h1>Dashboard</h1>
 
   <BSheetBottom :show="sheetBottomProfileShow" @close="onSheetBottomProfileToggle">
     <div class="px-5">
@@ -58,4 +91,21 @@ const onSheetBottomNotifToggle = () => {
     </div>
   </BSheetBottom>
 
+  <BCard v-for="i in 5" :key="i" class="pt-5">
+    <BCardContent>
+      <Bar
+        id="chart"
+        :options="chartOptions"
+        :data="chartData"
+      />
+    </BCardContent>
+  </BCard>
+
 </template>
+
+<style scoped>
+#chart {
+  width: 100% !important;
+  height: 400px !important;
+}
+</style>
