84127376354e431b9409b41a83f5e2bf326ab596
hadihammurabi <hadihammurabi@gmail.com>
2023-11-14 13:15:14 +0700

    feat(Button): with variant and size

--- uno.config.ts
+++ uno.config.ts
@@ -5,6 +5,15 @@ import presetWebFonts from '@unocss/preset-web-fonts'
 import transformerDirectives from '@unocss/transformer-directives';
 
 export default defineConfig({
+  theme: {
+    colors: {
+      primary: '#5463FF',
+      'primary-foreground': '#EEEEEE',
+      danger: '#FF1818',
+      'danger-foreground': '#EEEEEE',
+      black: '#222831',
+    },
+  },
   presets: [
     presetUno(),
     presetIcons(),
